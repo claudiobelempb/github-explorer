d395ea3a59e18bab4ccd23bcbc91be7cbecf9447
claudiobelempb <claudio.c.lima@hotmail.com>
2021-03-17 12:52:11 -0300

    Estado do componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './styles/global.scss';
 import { ListDefault } from './components/ListDefault';
+import { Counter } from './components/Counter'
+
 
 export function App() {
   return (
     <>
      <ListDefault title={'Lista de Repositórios'}/>
+     <Counter />
     </>
   )
 }
